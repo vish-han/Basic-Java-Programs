ce891eacbfdf3eb76a7cf43b0913401e2b233255
Vishal Chauhan <vshlchauhab@gmail.com>
2021-09-09 12:52:17 +0530

    searching Questions added

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+package Searching;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[]arr={2,34,45,123,452,12};
+        System.out.println(  lsearch(arr ,5));
+    }
+
+     static int lsearch(int[] arr, int num) {
+        int temp=num;
+        int i=0;
+        while(i< arr.length){
+            if(temp==arr[i]){
+                System.out.println("element found");
+            }
+
+            i++;
+            return i;
+        }
+return -1;
+}
+}
